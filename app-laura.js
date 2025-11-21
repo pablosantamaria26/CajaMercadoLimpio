539d4192e93e817c7ac270ecccb727cd93b5857d
pablosantamaria26 <santamariapablodaniel@gmail.com>
2025-11-21 13:03:52 -0300

    Update app-laura.js

--- app-laura.js
+++ app-laura.js
@@ -73,7 +73,7 @@ const EMPLEADOS = [
 // ===============================
 
 async function api(fn, params = {}) {
-  const res = await fetch(WORKER_URL, {
+  const res = await fetch(API_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ fn, params })
