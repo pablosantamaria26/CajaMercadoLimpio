c00ed645fe76c73fd2e7029509629a55d1568882
pablosantamaria26 <santamariapablodaniel@gmail.com>
2025-11-27 15:22:49 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker Básico para permitir instalación PWA
+const CACHE_NAME = 'caja-ml-v1';
+const urlsToCache = [
+  './',
+  './laura.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
